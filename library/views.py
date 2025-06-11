716764fb2708781f026f9db317de4cbad6082050
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-11 23:26:15 +0300

    Добавлены комментарии к произведениям: форма и вывод

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from .models import Work
 from django.contrib.auth.decorators import login_required
 from django import forms
 from django.shortcuts import get_object_or_404
+from .forms import CommentForm
 
 
 class WorkForm(forms.ModelForm):
@@ -35,3 +36,26 @@ def work_list_view(request):
 def work_detail_view(request, work_id):
     work = get_object_or_404(Work, id=work_id)
     return render(request, 'library/work_detail.html', {'work': work})
+
+
+@login_required
+def work_detail_view(request, work_id):
+    work = get_object_or_404(Work, id=work_id)
+    comments = work.comments.all().order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.work = work
+            comment.user = request.user
+            comment.save()
+            return redirect('work_detail', work_id=work.id)
+    else:
+        form = CommentForm()
+
+    return render(request, 'library/work_detail.html', {
+        'work': work,
+        'comments': comments,
+        'form': form
+    })
