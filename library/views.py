72a8c02aec7d77a0c0263426cdfec7250521de0b
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-11 17:23:07 +0300

    Добавлена публикация произведений, список и просмотр по ID

--- library/views.py
+++ library/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Work
+from django.contrib.auth.decorators import login_required
+from django import forms
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class WorkForm(forms.ModelForm):
+    class Meta:
+        model = Work
+        fields = ['title', 'genre', 'content']
+
+
+@login_required
+def create_work_view(request):
+    if request.method == 'POST':
+        form = WorkForm(request.POST)
+        if form.is_valid():
+            work = form.save(commit=False)
+            work.author = request.user
+            work.save()
+            return redirect('work_list')
+    else:
+        form = WorkForm()
+    return render(request, 'library/create_work.html', {'form': form})
+
+
+@login_required
+def work_list_view(request):
+    works = Work.objects.all().order_by('-published_date')
+    return render(request, 'library/work_list.html', {'works': works})
+
+
+@login_required
+def work_detail_view(request, work_id):
+    work = get_object_or_404(Work, id=work_id)
+    return render(request, 'library/work_detail.html', {'work': work})
