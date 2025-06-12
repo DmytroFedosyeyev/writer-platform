c36ad8be81fca482d910c2a9e05fc1130231a969
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-12 16:35:35 +0300

    Реализован поиск и фильтрация произведений по названию, жанру и автору

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from .models import Work
 from django.contrib.auth.decorators import login_required
 from django import forms
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 
 class WorkForm(forms.ModelForm):
@@ -35,3 +36,32 @@ def work_list_view(request):
 def work_detail_view(request, work_id):
     work = get_object_or_404(Work, id=work_id)
     return render(request, 'library/work_detail.html', {'work': work})
+
+
+@login_required
+def work_list_view(request):
+    title_query = request.GET.get('title', '')
+    genre_filter = request.GET.get('genre', '')
+    author_query = request.GET.get('author', '')
+
+    works = Work.objects.none()  # По умолчанию пустой список
+
+    # Если есть хотя бы один фильтр, применяем фильтрацию
+    if title_query or genre_filter or author_query:
+        works = Work.objects.all().order_by('-published_date')
+
+        if title_query:
+            works = works.filter(title__icontains=title_query)
+
+        if genre_filter:
+            works = works.filter(genre=genre_filter)
+
+        if author_query:
+            works = works.filter(author__username__icontains=author_query)
+
+    return render(request, 'library/work_list.html', {
+        'works': works,
+        'title_query': title_query,
+        'genre_filter': genre_filter,
+        'author_query': author_query,
+    })
