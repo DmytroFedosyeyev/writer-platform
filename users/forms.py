e4f68e9a9094ef701fd943f1630ba2df9796e077
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-18 23:49:10 +0300

    Добавлен функционал профиля: регистрация с именем/фамилией, личный кабинет, просмотр профилей авторов, ссылки на профили в списке произведений

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label="Имя")
+    last_name = forms.CharField(max_length=30, required=True, label="Фамилия")
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('home/', views.home_view, name='user_home'),
+    path('profile/<str:username>/', views.profile_view, name='user_profile'),
 ]
