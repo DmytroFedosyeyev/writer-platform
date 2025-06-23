8e8133421fd46e37d7d9d3833f16ddb752cf8dbd
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-23 16:38:07 +0300

    Fix: добавлены поля first_name и last_name в шаблон регистрации

--- users/forms.py
+++ users/forms.py
@@ -5,15 +5,17 @@ from django.contrib.auth.models import User
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True, label="Имя")
     last_name = forms.CharField(max_length=30, required=True, label="Фамилия")
+    email = forms.EmailField(required=True, label="Email")
 
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name')
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email')
 
     def save(self, commit=True):
         user = super().save(commit=False)
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
