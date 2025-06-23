fcd5d97d3da14d3576bb2424a5cf1177ec91af72
Dmytro Fedosyeyev <fedoseevdv1975@gmail.com>
2025-06-23 18:14:10 +0300

    Настроена админка для моделей Work, Comment и Rating

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Work, Comment, Rating
 
-# Register your models here.
+@admin.register(Work)
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'genre', 'published_date')
+    list_filter = ('genre', 'published_date')
+    search_fields = ('title', 'author__username', 'content')
+    ordering = ('-published_date',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'work', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('text', 'user__username', 'work__title')
+
+@admin.register(Rating)
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'work', 'score')
+    list_filter = ('score',)
+    search_fields = ('user__username', 'work__title')
